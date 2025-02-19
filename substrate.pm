0f1a9a707cb2cf01827c1382e02f88e905cb74e1
evan duffield <166574917+evanduffield9@users.noreply.github.com>
2025-02-18 17:29:11 -0700

    Create substrate.pm

---
+++ substrate.pm
@@ -0,0 +1,25 @@
+_(mbill) mbill
+  ai     ai 
+     bill      target 
+
+     continue   loop
+     loop
+loop
+
+as(mbill) tonight 
+   ha 
+ha 
+
+ha
+  continue 
+
+  loop
+ha 
+
+ha
+  loop
+ha 
+
+ha
+  hoop loop coop doop nope stupid coo lo
+lo
